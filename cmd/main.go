b57da80433d4be895a528b1d717459af66fd3766
ShaLeeZ2002 <108208048+Shavies@users.noreply.github.com>
2025-08-08 12:50:47 +0700

    add "corn job" to main

--- cmd/main.go
+++ cmd/main.go
@@ -2,20 +2,33 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/fleetcard/config"
 	"github.com/fleetcard/controllers"
 	"github.com/fleetcard/db"
+	"github.com/go-co-op/gocron"
 )
 
 func main() {
-
 	cfg, err := config.LoadConfig()
 	if err != nil {
 		log.Fatalf("Failed to load config: %v", err)
 	}
-	
+
 	db.DB = db.Connect(cfg)
 
-	controllers.ProcessAllInboundFiles(cfg.DateFormat)
+	s := gocron.NewScheduler(time.Local)
+
+	// รันทุกวันเวลา 05:00 AM
+	_, err = s.Every(1).Day().At("05:00").Do(func() {
+		log.Println("Running scheduled task at 05:00 AM")
+		controllers.ProcessAllInboundFiles(cfg.DateFormat)
+	})
+	if err != nil {
+		log.Fatalf("Scheduler setup failed: %v", err)
+	}
+
+	log.Println("Cron job scheduled. Waiting...")
+	s.StartBlocking()
 }
